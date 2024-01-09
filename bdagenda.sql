dfdadf50a6923bfe6acea3c9a3021857d974dd84
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-08 21:40:53 -0500

    Update bdagenda.sql

--- bdagenda.sql
+++ bdagenda.sql
@@ -12,8 +12,7 @@ CREATE TABLE agenda
   telefmovi                  VARCHAR(9),
   email                      VARCHAR(30),
   fbc                        VARCHAR(30),
-  CONSTRAINT agenda_codigoper_pk PRIMARY KEY (codigoper))
- );
+  CONSTRAINT agenda_codigoper_pk PRIMARY KEY (codigoper));
 
 INSERT INTO agenda VALUES ('1', 'carlos', 'serna','av. palmeras 145',
 '043524875', '942587568','carlos@hotmail.com', 'carlos serna');
